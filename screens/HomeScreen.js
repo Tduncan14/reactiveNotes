95156d1b7d69eea742d806c94108cea05117e56c
tduncan14 <nbatre1@yahoo.com>
2022-06-19 22:26:58 -0700

    keep moving forward

---
+++ screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React, {useEffect,useState,useContext} from 'react';
+import {Text,FlatList,TouchableOpacity} from 'react-native';
+import {Button} from 'native-base';
+import { Layout } from '../components/Layout';
+import { NoteContent } from '../components/Card';
+import { NoteContext } from '../contexts/NoteContext';
+
+
+
+
+const HomeScreen = props => {
+
+    const {getContextNotes} = userContext();
+    const [notes,setNotes] = useState();
+
+
+
+
+}
